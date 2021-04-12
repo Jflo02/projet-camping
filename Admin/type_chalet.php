203be54d5f831d95a6534c3cadf6456a5e685382
Piett-rm <rmpetit24@gmail.com>
2021-04-12 14:23:00 +0200

    type_admin

--- Admin/type_chalet.php
+++ Admin/type_chalet.php
@@ -52,27 +52,24 @@ switch ($_GET['c']) {
     
     case 'read':
 
-        $sql = "SELECT * FROM type_chalet WHERE id_etu=" . $_GET['id_etu'];
+        $sql = "SELECT * FROM type_chalet WHERE id_type_chalet=" . $_GET['id_type_chalet'];
         $resultat = mysqli_query($conn, $sql);
         if ($resultat == FALSE) {
             die("<br>Echec d'execution de la requete : " . $sql);
         } elseif (mysqli_num_rows($resultat) == 1) {
             $row = mysqli_fetch_assoc($resultat);
         ?>
-            <form action="./index.php" method="get">
-                <label for="nom">Nom :</label>
-                <input type="text" id="nom_etu" name="nom_etu" value="<?php echo $row['nom_etu'] ?>"><br><br>
-                <label for="prenom">Prénom :</label>
-                <input type="text" id="prenom_etu" name="prenom_etu" value="<?php echo $row['prenom_etu'] ?>"><br><br>
-                <label for="mail">Mail :</label>
-                <input type="text" id="mail_etu" name="mail_etu" value="<?php echo $row['mail_etu'] ?>"><br><br>
-                <label for="mail">Mot de passe :</label>
-                <input type="text" id="mdp_etu" name="mdp_etu" value="<?php echo $row['mdp_etu'] ?>"><br><br>
-                <input type="hidden" name="id_etu" value="<?php echo $row['id_etu'] ?>">
-                <input type="hidden" name="c" value="e">
-                <input type="hidden" name="do" value="update">
-                <input type="submit" value="Envoyer">
-            </form>
+            <form action="./type_chalet.php" method="get">
+                <label for="libelle">libelle :</label>
+                        <input type="text" id="libelle" name="libelle" value="<?php echo $row['libelle'] ?>"><br><br>
+                        <label for="prix_base">Prix de base :</label>
+                        <input type="text" id="prix_base" name="prix_base" value="<?php echo $row['prix_base'] ?>"><br><br>
+                        <label for="mail">nombre de place :</label>
+                        <input type="text" id="nb_place" name="nb_place" value="<?php echo $row['nb_place'] ?>"><br><br>
+                        <input type="hidden" name="id_type_chalet" value="<?php echo $row['id_type_chalet'] ?>">
+                        <input type="hidden" name="c" value="update">
+                        <input type="submit" value="Envoyer">
+                    </form>
         <?php
         }
         break;
@@ -84,7 +81,7 @@ switch ($_GET['c']) {
                 die("Il manque une valeur pour " . $key);
             }
         }
-        $sql = "UPDATE etudiants SET nom_etu='" . $_GET['nom_etu'] . "', prenom_etu='" . $_GET['prenom_etu'] . "',mail_etu='" . $_GET['mail_etu'] . "',mdp_etu='" . $_GET['mdp_etu'] . "' where id_etu=" . $_GET['id_etu'];
+        $sql = "UPDATE type_chalet SET libelle='" . $_GET['libelle'] . "', prix_base='" . $_GET['prix_base'] . "',nb_place='" . $_GET['nb_place'] . "' where id_type_chalet=" . $_GET['id_type_chalet'];
         $resultat = mysqli_query($conn, $sql);
         if ($resultat == FALSE) {
             die("<br>Echec d'execution de la requete : " . $sql);
@@ -94,6 +91,13 @@ switch ($_GET['c']) {
         break;
     
     case 'del':
+        $sql = "DELETE FROM type_chalet where id_type_chalet='" . $_GET['id_type_chalet']. "'";
+                        $resultat = mysqli_query($conn, $sql);
+                        if ($resultat == FALSE) {
+                            die("<br>Echec d'execution de la requete : " . $sql);
+                        } else {
+                            echo "Enregistrement supprimé";
+                        }
         break;
 
 
@@ -123,8 +127,8 @@ switch ($_GET['c']) {
                 echo "<td>" . $row['libelle'] . "</td>";
                 echo "<td>" . $row['prix_base'] . "</td>";
                 echo "<td>" . $row['nb_place'] . "</td>";
-                echo "<td><a href=./type_chalet.php?c=del&id_etu=" . $row['id_type_chalet'] . ">supprimer</a></td>";
-                echo "<td><a href=./type_chalet.php?c=read&id_etu=" . $row['id_type_chalet'] . ">éditer</a></td>";
+                echo "<td><a href=./type_chalet.php?c=del&id_type_chalet=" . $row['id_type_chalet'] . ">supprimer</a></td>";
+                echo "<td><a href=./type_chalet.php?c=read&id_type_chalet=" . $row['id_type_chalet'] . ">éditer</a></td>";
                 }
                 echo "</tr>";
             }
